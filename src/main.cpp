48048c37e0b4be392585ff10f3b080f97a6fbc70
shewitt.au <shewitt.au@gmail.com>
2025-05-02 22:50:18 +1000

    Fix build issue. Work on gdb config

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,40 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+template <class T>
+struct Mallocator
+{
+    typedef T value_type;
+    Mallocator() noexcept {}
+
+    template<class U> Mallocator(const Mallocator<U>&) noexcept {}
+    template<class U> bool operator==(const Mallocator<U>&) const noexcept
+    {
+        return true;
+    }
+    template<class U> bool operator!=(const Mallocator<U>&) const noexcept
+    {
+        return false;
+    }
+    T* allocate(const size_t n) const
+    {
+        return new T();
+    }
+    void deallocate(T* const p, size_t) const noexcept
+    {
+        delete p;
+    }
+};
+
 int main()
 {
+ //   *(int*)0 = 0;
+
+    cout << "Made it" << endl;
+
+    vector<int, Mallocator<int>> v;
+    v.push_back(3);
+
     return 0;
 }
