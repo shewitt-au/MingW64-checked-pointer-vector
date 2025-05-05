7890fd057e86a0aa6fbdc507922ce46b02f3e261
shewitt.au <shewitt.au@gmail.com>
2025-05-06 01:19:16 +1000

    So far so good
    Stripped out some old code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <vector>
+#include <algorithm>
 #include <iostream>
+#include <cstdint>
+#include <windows.h>
+#include "WriteWatchDLL.h"
 using namespace std;
 
 template <class T>
@@ -8,31 +12,34 @@ class checked_alloc
 public:
     typedef T value_type;
     checked_alloc() noexcept {}
-
+    ~checked_alloc() noexcept {}
     template<class U> checked_alloc(const checked_alloc<U>&) noexcept {}
+    
     template<class U> bool operator==(const checked_alloc<U>&) const noexcept
     {
         return true;
     }
-    template<class U> bool operator!=(const checked_alloc<U>&) const noexcept
-    {
-        return false;
-    }
+
     T* allocate(const size_t n) const
     {
-        return static_cast<T*>(::operator new(n*sizeof(T)));
+        return static_cast<T*>(AllocWatched(n*sizeof(T)));
+        //return static_cast<T*>(::operator new(n*sizeof(T)));
     }
-    void deallocate(T* const p, size_t) const noexcept
+
+    void deallocate(T* const p, size_t n) const noexcept
     {
-        ::operator delete(p);
+        FreeWatched(p);
+        //::operator delete(p); //FreeWatched(p);
     }
 };
 
 int main()
 {
-    vector<int, checked_alloc<int>> v = {3, 1, 4, 1, 5, 9, 2, 6, 5};
+    // -exec handle SIGSEGV nostop noprint pass
+
+   vector<int, checked_alloc<int>> v = {3, 1, 4, 1, 5, 9, 2, 6, 5};
     for (auto n : v)
-    cout << n << endl;
+        cout << n << endl;
 
     return 0;
 }
